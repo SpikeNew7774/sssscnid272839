21ed2405f53cf87504b04e09f0e9b17ac7f13b05
SpikeNew7774 <88393630+SpikeNew7774@users.noreply.github.com>
2023-03-29 16:34:27 +0200

    Add files via upload

---
+++ ggg-spedup-scptb.js
@@ -0,0 +1,26 @@
+if (!mainVideo.src) {
+    document.getElementById("loadgif").style.display = "block"
+  }
+  
+  
+  
+  
+  fetch('https://spikenew7774.github.io/sss-stream-vids/ggg-vid/gggvid.mp4')
+      .then(res => res.blob())
+      .then(res => generateURL(res))
+  
+  function generateURL(blob) {
+  
+      const url = URL.createObjectURL(blob);
+  
+      mainVideo.src = url
+      mainVideo.textContent = "";
+      mainVideo.style.display = "block"
+  
+      if (mainVideo.src == url) {
+        document.getElementById("loadgif").style.display = "none"
+        document.getElementById("hst").style.display = "none"
+  
+      }
+  
+  };
